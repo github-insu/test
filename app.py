5e1507284b4f0b2d7a4355ea98178f99a163e6f8
github-insu <121149852+github-insu@users.noreply.github.com>
2023-01-07 00:58:54 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
+# HTML,CSS,JAVASCRIPT 없이 PYTHON만으로 웹 페이지 만들기
 import streamlit as st
+
+#raw data
 view = [100,150,30]
+
+#HTML에서 H태그 역할
 st.write('# Youtube. view')
 st.write('## raw')
 view
-st.wirte('## bar char')
-st.bar_char(view)
+st.write('## bar char')
+
+#bar_chart 표시
+st.bar_chart(view)
 
 import pandas as pd
+
+#pandas의 Series를 바로 보여줄 수 있음
 sview = pd.Series(view)
 sview
