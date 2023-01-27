9897fd21b891e72125c87e657bfb507fb11c8669
github-insu <93insu@gmail.com>
2023-01-27 21:27:47 +0900

    spring_from_front_to_back

--- test/src/main/java/insu/test/controller/MainController.java
+++ test/src/main/java/insu/test/controller/MainController.java
@@ -2,6 +2,7 @@ package insu.test.controller;
 
 import insu.test.dto.InfoDto;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -14,9 +15,15 @@ public class MainController {
     }
 
     @PostMapping("/info/create")
-    public String createInfo(InfoDto form){
-        System.out.println(form.toString());
-        return "";
-    }
+    public String createInfo(InfoDto form, Model model){
+        System.out.println(model);
+        System.out.println(form);
+        model.addAttribute("id",model.getAttribute("id"));
+        model.addAttribute("id", form.getId());
+        model.addAttribute("pw", form.getPw());
+        model.addAttribute("email",form.getEmail());
+        model.addAttribute("phone_number",form.getPhone_number());
 
+        return "info_create";
+    }
 }
--- test/src/main/java/insu/test/dto/InfoDto.java
+++ test/src/main/java/insu/test/dto/InfoDto.java
@@ -1,32 +1,39 @@
 package insu.test.dto;
 
-// info html 에서 form 태그에 작성한 데이터를 나르기 위한 객체
 public class InfoDto {
-
-    //각 변수 선언
+    //form 태그에서 보내는 데이터를 각각 변수화
     private String id;
     private String pw;
     private String email;
     private String phone_number;
 
-    //각 변수에 대한 생성자 선언 --> DTO에 담아 보내는 각각의 변수들을 하나로 객체화
-    public InfoDto(String id, String pw, String email, String phone_number) {
+
+    // 각 변수들을 getter & setter
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
         this.id = id;
-        this.pw = pw;
-        this.email = email;
-        this.phone_number = phone_number;
     }
 
-    //toString() 코드 --> JSON 양식처럼 출력하게 하는 코드
-    //Override는 왜 하는거야?
+    public String getPw() {
+        return pw;
+    }
+    public void setPw(String pw) {
+        this.pw = pw;
+    }
 
-    @Override
-    public String toString() {
-        return "InfoDto{" +
-                "id='" + id + '\'' +
-                ", pw='" + pw + '\'' +
-                ", email='" + email + '\'' +
-                ", phone_number='" + phone_number + '\'' +
-                '}';
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone_number() {
+        return phone_number;
+    }
+    public void setPhone_number(String phone_number) {
+        this.phone_number = phone_number;
     }
 }
