4d7d5967827d7ecced94c25f5101b6d92a04fc2d
github-insu <93insu@gmail.com>
2023-03-06 10:53:05 +0900

    opencv_commit

--- youtube/main.js
+++
@@ -1,7 +0,0 @@
-const moreBtn = doument.querySelector('.info .metadata .moreBtn');
-const title = document.querySelector('.info .metadata .title');
-
-moreBtn.anddEventListener('click', () => {
-    moreBtn.classList.toggle('clicked');
-    title.classList.toggle('clamp');
-});
